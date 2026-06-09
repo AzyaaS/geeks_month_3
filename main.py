e5561e36bb20b4e5eb2758212e4da30cfa953ef0
Azyaa <saaz.sr.rs@gmail.com>
2026-06-09 15:17:13 +0600

    добавил  функцию проверки возраста, под нее сделал поле ввода, кнопку для отправки и текст где выводиться результат

--- main.py
+++ main.py
@@ -11,10 +11,11 @@ def main(page: ft.Page):
     favorite_names = []
 
     count = 0
-
+    
     greeting_text = ft.Text("История приветствий")
     favorite_names_text = ft.Text("Любимые имена: \n")
     click_count_text = ft.Text(f"количество нажатий кнопки SEND: {count}")
+    access_age_text = ft.Text(value="",size=25)
 
     def text_name(e):   # e - event
         name = text_input.value.strip()
@@ -42,6 +43,8 @@ def main(page: ft.Page):
     text_input = ft.TextField(label="Введите свое имя", on_submit=text_name, expand=False)
     btn = ft.ElevatedButton('SEND', on_click=text_name)
     
+
+
     def clear_history():
         greeting_history.clear()
         greeting_text.value = f"История приветствий: "
@@ -70,18 +73,43 @@ def main(page: ft.Page):
             page.theme_mode = ft.ThemeMode.LIGHT
         else:
             page.theme_mode = ft.ThemeMode.DARK
-        
+    def check_age(e):
+        age = text_age_input.value.strip()
+
+        if age.isdigit():
+            age_number = int(age)
+            if age_number >= 18:
+                access_age_text.value = "Доступ разрешен"
+                access_age_text.color = ft.Colors.GREEN
+            else:
+                access_age_text.value = "Доступ запрещен"
+                access_age_text.color = ft.Colors.RED
+        else:
+            access_age_text.value = "Введите корректный возраст"
+            access_age_text.color = ft.Colors.YELLOW
+        text_age_input.value = ""
+        page.update()
+    age_send_button = ft.IconButton(icon=ft.Icons.SEND, on_click=check_age)
+    text_age_input = ft.TextField(label="Введите свой возраст", on_submit=check_age)
     
+
     theme_btn = ft.IconButton(icon=ft.Icons.BRIGHTNESS_7, on_click=thememode)
 
     main_object = ft.Row(
         controls=[text_input,btn,clear_button,favorite_name_button], 
         alignment=ft.MainAxisAlignment.CENTER)
     text_row = ft.Row(
-        controls=[text_hello],
+        controls=[text_hello,],
         alignment=ft.MainAxisAlignment.CENTER
     )
-
-    page.add(text_row, main_object, theme_btn, greeting_text,favorite_names_text, click_count_text )
+    age_acces_row = ft.Row(
+        controls=[access_age_text],
+        alignment=ft.MainAxisAlignment.CENTER
+    )
+    age_row = ft.Row(
+        controls= [text_age_input,age_send_button],
+        alignment=ft.MainAxisAlignment.CENTER
+    )
+    page.add(text_row, age_acces_row, main_object, age_row, theme_btn, greeting_text,favorite_names_text, click_count_text )
 
 ft.app(target=main, view=ft.AppView.WEB_BROWSER)
